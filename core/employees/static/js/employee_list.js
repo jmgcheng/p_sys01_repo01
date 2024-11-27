739d86f810622b4ef153a10942a929532b1e2ada
jcheng-servicesolutions <jcheng@servicesolutions.biz>
2024-11-27 12:28:23 +0800

    add - functinality - employee list - multiple checkbox filter

--- core/employees/static/js/employee_list.js
+++ core/employees/static/js/employee_list.js
@@ -46,6 +46,31 @@ $(document).ready(function() {
         pageLength: 25,
     });
 
+    $('input[type=checkbox][name=chkPosition], input[type=checkbox][name=chkSpecialty], input[type=checkbox][name=chkGender], input[type=checkbox][name=chkEmployeeStatus]').change(function() {
+        let groupName = $(this).attr("name");
+        // get the master checkbox for this group
+        let groupAllCheckbox = $('#'+groupName+'-0');
+        // 
+        // get the other group checkbox that is not master 
+        let otherCheckboxes = $('input[type=checkbox][name='+groupName+']').not(groupAllCheckbox);
+        
+        // is this the master your changing?
+        if ($(this).is(groupAllCheckbox)) {
+            if (groupAllCheckbox.prop('checked')) {
+                otherCheckboxes.prop('checked', true);
+            } else {
+                otherCheckboxes.prop('checked', false);
+            }
+        } else {
+            if (otherCheckboxes.filter(':checked').length === otherCheckboxes.length) {
+                groupAllCheckbox.prop('checked', true);
+            } else {
+                groupAllCheckbox.prop('checked', false);
+            }
+        }
+        table.draw();
+    });
+
     function getCheckedValues(name) {
         var values = [];
         $('input[name="' + name + '"]:checked').each(function() {
