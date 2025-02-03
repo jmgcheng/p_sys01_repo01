f374995d386cb87d36353a4bf4b5a0d81d1ebcce
jcheng-servicesolutions <jcheng@servicesolutions.biz>
2025-02-03 13:28:08 +0800

    update - generate dummy transactions

--- core/management_commands/management/commands/generate_excel_dummy_transactions.py
+++ core/management_commands/management/commands/generate_excel_dummy_transactions.py
@@ -17,15 +17,211 @@ class Command(BaseCommand):
 
     def setup_defaults(self):
         self.start_loop_date = date(2025, 1, 1)
-        self.end_loop_date = date(2025, 2, 1)
+        self.end_loop_date = date(2025, 12, 30)
         self.current_date = self.start_loop_date
         self.product_variation_list = [
             {'code': 'PV-001', 'current_quantity': 0,
-                'quantity_alert': 100, 'arriving_quantity': 0},
+                'quantity_alert': 300, 'arriving_quantity': 0},
             {'code': 'PV-002', 'current_quantity': 0,
-                'quantity_alert': 100, 'arriving_quantity': 0},
+                'quantity_alert': 300, 'arriving_quantity': 0},
             {'code': 'PV-003', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-004', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-005', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-006', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-007', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-008', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-009', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-010', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-011', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-012', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-013', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-014', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-015', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-016', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-017', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-018', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-019', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-020', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-021', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-022', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-023', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-024', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-025', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-026', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-027', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-028', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-029', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-030', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-031', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-032', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-033', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-034', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-035', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-036', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-037', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-038', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-039', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-040', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-041', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-042', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-043', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-044', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-045', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-046', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-047', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-048', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-049', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-050', 'current_quantity': 0,
+                'quantity_alert': 200, 'arriving_quantity': 0},
+            {'code': 'PV-051', 'current_quantity': 0,
+                'quantity_alert': 200, 'arriving_quantity': 0},
+            {'code': 'PV-052', 'current_quantity': 0,
+                'quantity_alert': 200, 'arriving_quantity': 0},
+            {'code': 'PV-053', 'current_quantity': 0,
+                'quantity_alert': 200, 'arriving_quantity': 0},
+            {'code': 'PV-054', 'current_quantity': 0,
+                'quantity_alert': 200, 'arriving_quantity': 0},
+            {'code': 'PV-055', 'current_quantity': 0,
+                'quantity_alert': 200, 'arriving_quantity': 0},
+            {'code': 'PV-056', 'current_quantity': 0,
+                'quantity_alert': 200, 'arriving_quantity': 0},
+            {'code': 'PV-057', 'current_quantity': 0,
                 'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-058', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-059', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-060', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-061', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-062', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-063', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-064', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-065', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-066', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-067', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-068', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-069', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-070', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-071', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-072', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-073', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-074', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-075', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-076', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-077', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-078', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-079', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-080', 'current_quantity': 0,
+                'quantity_alert': 100, 'arriving_quantity': 0},
+            {'code': 'PV-081', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-082', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-083', 'current_quantity': 0,
+                'quantity_alert': 300, 'arriving_quantity': 0},
+            {'code': 'PV-084', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-085', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-086', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-087', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-088', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-089', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-090', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-091', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-092', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-093', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-094', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-095', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-096', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-097', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-098', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-099', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-100', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
+            {'code': 'PV-101', 'current_quantity': 0,
+                'quantity_alert': 500, 'arriving_quantity': 0},
         ]
         self.todays_request_product_variations = []
         self.purchase_request_header_list = []
@@ -36,11 +232,15 @@ class Command(BaseCommand):
         self.sale_invoice_detail_list = []
         self.official_receipt_header_list = []
         self.official_receipt_detail_list = []
-        self.purchase_requestor_list = ['TEST-ID-14', 'TEST-ID-15']
-        self.purchase_approver_list = ['TEST-ID-14', 'TEST-ID-15']
-        self.sale_representative_list = ['TEST-ID-14', 'TEST-ID-15']
-        self.vendor_list = ['VENDOR-01', 'VENDOR-02']
-        self.customer_list = ['CUST-01', 'CUST-02', 'CUST-03']
+        self.purchase_requestor_list = ['EMP-ID-25', 'EMP-ID-26', 'EMP-ID-27']
+        self.purchase_approver_list = [
+            'EMP-ID-27', 'EMP-ID-20', 'EMP-ID-21', 'EMP-ID-22', 'EMP-ID-23', 'EMP-ID-24']
+        self.sale_representative_list = [
+            'EMP-ID-28', 'EMP-ID-29', 'EMP-ID-30', 'EMP-ID-31', 'EMP-ID-32']
+        self.vendor_list = ['VENDOR-01', 'VENDOR-02', 'VENDOR-03',
+                            'VENDOR-04', 'VENDOR-05', 'VENDOR-06', 'VENDOR-07']
+        self.customer_list = ['CUST-ID-01', 'CUST-ID-02', 'CUST-ID-03', 'CUST-ID-04', 'CUST-ID-05', 'CUST-ID-06', 'CUST-ID-07', 'CUST-ID-08', 'CUST-ID-09', 'CUST-ID-10', 'CUST-ID-11', 'CUST-ID-12', 'CUST-ID-13', 'CUST-ID-14',
+                              'CUST-ID-15', 'CUST-ID-16', 'CUST-ID-17', 'CUST-ID-18', 'CUST-ID-19', 'CUST-ID-20', 'CUST-ID-21', 'CUST-ID-22', 'CUST-ID-23', 'CUST-ID-24', 'CUST-ID-25', 'CUST-ID-26', 'CUST-ID-27', 'CUST-ID-28', 'CUST-ID-29', 'CUST-ID-30']
         self.sale_category_list = ['RETAIL', 'PATIENT',
                                    'WHOLESALE', 'GOVERNMENT', 'CORPORATE']
 
@@ -126,10 +326,21 @@ class Command(BaseCommand):
 
         for purchase_request_detail in self.purchase_request_detail_list:
             if purchase_request_detail['purchase_request_header'] == purchase_request_header['code']:
+
+                # create random issue in quantity_received to simulate real life
+                quantity_received = 0
+                issue_is = randint(1, 10)
+                if issue_is <= 2:  # 35% chance of issue
+                    print('created issue in purchase receive detail')
+                    quantity_received = randint(
+                        purchase_request_detail['quantity_request'] - 90, purchase_request_detail['quantity_request'] + 100)
+                else:
+                    quantity_received = purchase_request_detail['quantity_request']
+
                 detail_dict = {
                     'purchase_receive_header': purchase_receive_header['code'],
                     'product_variation': purchase_request_detail['product_variation'],
-                    'quantity_received': purchase_request_detail['quantity_request']
+                    'quantity_received': quantity_received
                 }
 
                 detail_list.append(detail_dict)
@@ -184,7 +395,7 @@ class Command(BaseCommand):
 
     def create_sale_invoice_detail(self, sale_invoice_header):
         detail_list = []
-        num_of_items_sold = randint(1, 3)
+        num_of_items_sold = randint(1, 5)
 
         for x in range(1, num_of_items_sold + 1):
             product_variation = random.choice(self.product_variation_list)
@@ -224,6 +435,16 @@ class Command(BaseCommand):
 
         for sale_invoice_detail in self.sale_invoice_detail_list:
             if sale_invoice_detail['sale_invoice_header'] == sale_invoice_header['code']:
+                # create random issue in quantity_paid to simulate real life
+                quantity_paid = 0
+                issue_is = randint(1, 10)
+                if issue_is <= 1:  # 10% chance of issue
+                    print('created issue in official receipt detail')
+                    quantity_paid = randint(
+                        sale_invoice_detail['quantity_request'] - 10, sale_invoice_detail['quantity_request'] + 20)
+                else:
+                    quantity_paid = sale_invoice_detail['quantity_request']
+
                 detail_dict = {
                     'official_receipt_header': official_receipt_header['code'],
                     'product_variation': sale_invoice_detail['product_variation'],
@@ -277,10 +498,13 @@ class Command(BaseCommand):
             #
             self.create_purchase_receive_transactions()
 
-            #
-            self.create_sale_invoice_transactions()
-            #
-            self.create_official_receipt_transactions()
+            # add random customer transaction
+            num_customer_today = randint(5, 10)
+            for x in range(1, num_customer_today + 1):
+                #
+                self.create_sale_invoice_transactions()
+                #
+                self.create_official_receipt_transactions()
 
             #
             self.reset_product_variant_arriving_quantity()
